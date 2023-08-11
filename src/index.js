bd98d5dd30459d325927ed16d0343cbd0260ffb6
Dmitrii Barilkin <dimabaril@gmail.com>
2023-08-11 03:26:35 +0300

    it works by stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2) {
+        return false;
+    }
+    let inArr = str.split("");
+    console.log(inArr);
+    let acc = [];
+    for (i = 0; i < inArr.length; i++) {
+        for (j = 0; j < bracketsConfig.length; j++) {
+            if (
+                inArr[i] === bracketsConfig[j][0] &&
+                bracketsConfig[j][0] === bracketsConfig[j][1]
+            ) {
+                if (inArr[i] === acc[acc.length - 1]) {
+                    acc.pop();
+                } else {
+                    acc.push(inArr[i]);
+                }
+            } else if (inArr[i] === bracketsConfig[j][0]) {
+                acc.push(inArr[i]);
+                console.log(acc);
+            } else if (
+                inArr[i] === bracketsConfig[j][1] &&
+                acc[acc.length - 1] === bracketsConfig[j][0]
+            ) {
+                acc.pop();
+                console.log(acc);
+            } else if (inArr[i] === bracketsConfig[j][1] && acc.length === 0) {
+                return false;
+            }
+        }
+    }
+    return !Boolean(acc.length);
+};
